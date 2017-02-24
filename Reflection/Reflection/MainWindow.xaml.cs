da8f7d39319842c48070587dabe99bdbd734f046
MINSK\Iryna_Bahatka1 <irynabahatka95@gmail.com>
2017-02-24 13:45:17 +0300

    Added button to choose directory

--- Reflection/Reflection/MainWindow.xaml.cs
+++ Reflection/Reflection/MainWindow.xaml.cs
@@ -20,9 +20,39 @@ namespace Reflection
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string filename;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.FileName = "Document"; // Default file name
+            dlg.DefaultExt = ".txt"; // Default file extension
+            dlg.Filter = "Text documents (.txt)|*.txt"; // Filter files by extension
+
+            // Show open file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                 filename = dlg.FileName;
+            }
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            filename.
         }
     }
 }
